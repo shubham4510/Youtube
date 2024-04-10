81c184a58f93dfe5dfdcef9fc608c573852071df
Shubham <shubhambhardwaj112277@gmail.com>
2024-04-10 23:00:45 +0530

    Implemented navigation and sidebar components

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import SideBar from './SideBar.jsx'
+
+const Body = () => {
+  return (
+    <div>
+        <SideBar/>
+    </div>
+  )
+}
+
+export default Body
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const SideBar = () => {
+  return (
+    <div className=' w-[15%] h-screen  flex flex-col px-3'>
+            <h3 className=' font-medium text-xl    mt-2'>Home</h3>
+        <ul>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>Shorts</li>
+            <li className=' opacity-80 text-xl  cursor-pointer border-b-2 border-gray-300 pb-2  mt-2'>Subscriptions</li>
+        </ul>
+            <h3 className=' font-medium text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>You </h3>
+        <ul>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>Your channel            </li>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>History     </li>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>Playlists          </li>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>Your videos         </li>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>Watch later    </li>
+            <li className=' opacity-80 text-xl cursor-pointer border-b-2 border-gray-300 pb-2   mt-2'>Liked videos        </li>
+        </ul>
+          
+    </div>
+  )
+}
+
+export default SideBar
